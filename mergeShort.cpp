3585f824572d0c0c67106ee80ca09e5161981f87
Abinayanafaiq <abinayanafaiq550@gmail.com>
2025-04-17 11:31:39 +0700

    first commit

--- mergeShort.cpp
+++ mergeShort.cpp
@@ -1,30 +1,5 @@
 #include <iostream>
 using namespace std;
-int array[]
 
-
-
-void input(){
-    while(true){
-        cout << "masukan Panjang element array";
-        cin >> n;
-
-        if (n<=20){
-            break;
-        }
-        else {
-            cout << "\nmaksimal panjang array adalah 20\n";
-        }
-    }
-
-    cout << "\n=====================";
-    cout << "\nInputkan isi element array";
-    cout << "\n====================="; << endl;
-
-
-
-    for (int i=0; i<n; i++){
-        cout << "element ke-" << i+1 << " : ";
-        cin >> array[i];
-    }
-}
+int arr[20], B[];   // Ukuran array maksimum 20
+int n;           // Variabel untuk panjang array
